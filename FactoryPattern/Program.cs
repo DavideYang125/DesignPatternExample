c51107949e3881b97f88e98eaf67cd97648dc550
yangww <1483722265@qq.com>
2019-10-17 20:56:22 +0800

    init

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    /// <summary>
+    /// 工厂设计模式
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
